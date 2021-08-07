422d12bcb71ffa206f3898124e4a18a8267265d2
braga <lucas.b.moura@ufv.br>
2021-08-07 13:43:59 -0300

    Updating the code from practice.

--- pratica3/buscaBinaria3.cpp
+++ pratica3/buscaBinaria3.cpp
@@ -18,14 +18,16 @@ using namespace std;
 int buscaBin(int *array,int begin, int end, int chave) {
 	if (begin > end) return -1;
 	int meio = (end-begin)/2 + begin;
-	if (array[meio] == chave)
-		return meio;
+
+	while(array[meio-1] == chave){
+		return meio-1;
+	}
+	
 	if (array[meio] > chave)
 		return buscaBin(array,begin, meio-1, chave); 
-	if(array[meio] >= chave)
-		return buscaBin(array,begin,meio,chave);
 	return buscaBin(array,meio+1, end, chave); 	
 }
+
 
 //Busca sequencial
 //Pesquisa pelo elemento "chave" entre as posicoes begin e end (inclusive) do array 
